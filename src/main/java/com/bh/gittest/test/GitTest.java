7b878d303c4fb8c7422617509b3447e1c0ef704f
wfk <wfk@123.com>
2022-07-30 23:51:50 +0800

    second

--- src/main/java/com/bh/gittest/test/GitTest.java
+++ src/main/java/com/bh/gittest/test/GitTest.java
@@ -3,5 +3,6 @@ package com.bh.gittest.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("dddddddddd");
+        System.out.println("cccccccccc");
     }
 }
